b3936aea296afe9fa7ab993b4dfb48cf7fcf815b
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2023-08-31 22:01:37 -0300

    inicia a página de login

---
+++ src/app/(public)/login/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/Form/index.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+
+
+export const Form = styled.form`
+    width: 60%;
+    margin: auto;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+`
